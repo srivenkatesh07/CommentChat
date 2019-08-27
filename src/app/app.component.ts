1dedbe7637d9c40f2b820b107288590e8926aa96
Venkatesh S <sr.venkatesh1324@gmail.com>
2019-08-27 11:18:33 +0530

    first 18

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { LoglistService } from './loglist.service';
 
 @Component({
+  moduleId:module.id,
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
